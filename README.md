a4205ee330b348b3c44e7c57219918b47d5f2609
VNMabus <vnmabus@gmail.com>
2017-09-27 09:54:49 +0200

    Improved metadata and readme

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # dcor
-Distance correlation and distance covariance in Python
+
+|build-status| |docs|
+
+dcor: distance correlation and distance covariance in Python.
+
+Distance covariance and distance correlation are
+dependency measures between random vectors introduced in 
+[doi:10.1214/009053607000000505](doi:10.1214/009053607000000505).
+
+This package offers functions for calculating several statistics
+related with distance covariance and distance correlation, including
+biased and unbiased estimators of both dependency measures.
+
+The documentation can be found in https://dcor.readthedocs.io/en/latest/?badge=latest
+
+.. |build-status| image:: https://api.travis-ci.org/vnmabus/dcor.svg?branch=master
+    :alt: build status
+    :scale: 100%
+    :target: https://travis-ci.org/vnmabus/dcor
+
+.. |docs| image:: https://readthedocs.org/projects/dcor/badge/?version=latest
+    :alt: Documentation Status
+    :scale: 100%
+    :target: https://dcor.readthedocs.io/en/latest/?badge=latest
--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
 # encoding: utf-8
 
-'''
-Created on 13 sept. 2017
+"""dcor: distance correlation and distance covariance in Python.
 
-@author: Carlos Ramos Carreño
-'''
+Distance covariance and distance correlation are
+dependency measures between random vectors introduced in [SRB07]_.
+
+This package offers functions for calculating several statistics
+related with distance covariance and distance correlation, including
+biased and unbiased estimators of both dependency measures.
+
+.. rubric:: References
+
+.. [SRB07] Gábor J. Székely, Maria L. Rizzo, and Nail K. Bakirov. Measuring and
+           testing dependence by correlation of distances. The Annals of
+           Statistics, 35(6):2769–2794, 12 2007.
+           doi:10.1214/009053607000000505.
+"""
 
 import os
 from setuptools import setup, find_packages
+
+DOCLINES = (__doc__ or '').split("\n")
 
 with open(os.path.join(os.path.dirname(__file__),
                        'VERSION'), 'r') as version_file:
@@ -15,13 +28,31 @@ with open(os.path.join(os.path.dirname(__file__),
 
 setup(name='dcor',
       version=version,
-      description='Distance correlation and distance covariance in Python',
+      description=DOCLINES[0],
+      long_description="\n".join(DOCLINES[2:]),
       url='https://github.com/vnmabus/dcor',
       author='Carlos Ramos Carreño',
       author_email='vnmabus@gmail.com',
       include_package_data=True,
+      platforms=['any'],
       license='MIT',
       packages=find_packages(),
+      python_requires='>=2.7, >=3.5, <4',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
       setup_requires=['pytest-runner'],
       tests_require=['pytest'],
       test_suite='dcor.tests',
