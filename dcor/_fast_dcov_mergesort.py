5074e6523f21e4505ee3f7536faaf7b62b901885
vnmabus <vnmabus@gmail.com>
2020-02-12 13:53:49 +0100

    Renamed variables

--- dcor/_fast_dcov_mergesort.py
+++ dcor/_fast_dcov_mergesort.py
@@ -16,52 +16,67 @@ def _compute_d(y, weights):
     indexes = np.arange(2 * n_samples).reshape((2, n_samples))
     indexes[1] = 0  # Remove this
 
-    i = 1
-    previous_idx = 1
-    current_idx = 2
+    previous_indexes = indexes[0]
+    current_indexes = indexes[1]
 
-    while i < n_samples:
-        gap = 2 * i
-        k = 0
-        previous_indexes = indexes[previous_idx]
+    merged_subarray_len = 1
+
+    # For all lengths that are a power of two
+    while merged_subarray_len < n_samples:
+        gap = 2 * merged_subarray_len
+        indexes_idx = 0
         weights_cumsum = np.cumsum(weights[previous_indexes])
         weights_cumsum = np.concatenate(([0], weights_cumsum))
 
-        j = 1
-        while j < n_samples:
-            st1 = j
-            st2 = j + i
-            e1 = min(st1 + i - 1, n_samples)
-            e2 = min(st2 + i - 1, n_samples)
-            e1_plus_1 = e1 + 1
+        # Select the subarrays in pairs
+        subarray_pair_idx = 0
+        while subarray_pair_idx < n_samples:
+            subarray_1_idx = subarray_pair_idx
+            subarray_2_idx = subarray_pair_idx + merged_subarray_len
+            subarray_1_idx_last = min(
+                subarray_1_idx + merged_subarray_len - 1, n_samples - 1)
+            subarray_2_idx_last = min(
+                subarray_2_idx + merged_subarray_len - 1, n_samples - 1)
+            e1_plus_1 = subarray_1_idx_last + 1
 
-            while st1 <= e1 and st2 <= e2:
-                k += 1
-                idx1 = previous_indexes[st1]
-                idx2 = previous_indexes[st2]
+            # Merge the subarrays
+            while (subarray_1_idx <= subarray_1_idx_last and
+                   subarray_2_idx <= subarray_2_idx_last):
+                previous_index_1 = previous_indexes[subarray_1_idx]
+                previous_index_2 = previous_indexes[subarray_2_idx]
 
-                if y[idx1] > y[idx2]:
-                    indexes[current_idx, k] = idx1
-                    st1 += 1
+                if y[previous_index_1] > y[previous_index_2]:
+                    current_indexes[indexes_idx] = previous_index_1
+                    subarray_1_idx += 1
                 else:
-                    indexes[current_idx, k] = idx2
-                    st2 += 1
+                    current_indexes[indexes_idx] = previous_index_2
+                    subarray_2_idx += 1
 
-                    d[idx2] += (weights_cumsum[e1_plus_1] -
-                                weights_cumsum[st1])
+                    d[previous_index_2] += (weights_cumsum[e1_plus_1] -
+                                            weights_cumsum[subarray_1_idx])
+                indexes_idx += 1
 
-            if st1 <= e1:
-                kf = k + e1 - st1 + 1
-                indexes[current_idx, (k + 1):kf] = previous_indexes[:, st1:e1]
-                k = kf
-            elif st2 <= e2:
-                kf = k + e2 - st2 + 1
-                indexes[current_idx, (k + 1):kf] = previous_indexes[:, st2:e2]
-                k = kf
+            # Join the remaining elements of one of the arrays (already sorted)
+            if subarray_1_idx <= subarray_1_idx_last:
+                n_remaining = subarray_1_idx_last - subarray_1_idx + 1
+                indexes_idx_next = indexes_idx + n_remaining
+                current_indexes[indexes_idx:indexes_idx_next] = (
+                    previous_indexes[subarray_1_idx:subarray_1_idx_last + 1])
+                indexes_idx = indexes_idx_next
+            elif subarray_2_idx <= subarray_2_idx_last:
+                n_remaining = subarray_2_idx_last - subarray_2_idx + 1
+                indexes_idx_next = indexes_idx + n_remaining
+                current_indexes[indexes_idx:indexes_idx_next] = (
+                    previous_indexes[subarray_2_idx:subarray_2_idx_last + 1])
+                indexes_idx = indexes_idx_next
 
-            j += gap
+            subarray_pair_idx += gap
 
-        i = gap
+        merged_subarray_len = gap
 
         # Swap buffer
-        previous_idx, current_idx = (current_idx, previous_idx)
+        previous_indexes, current_indexes = (current_indexes, previous_indexes)
+
+        print(previous_indexes + 1)
+
+    return d
