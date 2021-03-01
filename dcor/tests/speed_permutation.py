9d41ebf20fd6e82518bbdf7c3e8fb4e8110616e3
Michael Milton <ttmigueltt@gmail.com>
2021-03-01 18:35:05 +1100

    Add __main__ guard

--- dcor/tests/speed_permutation.py
+++ dcor/tests/speed_permutation.py
@@ -2,15 +2,20 @@ import numpy as np
 import dcor
 import sys
 
-resamples = int(sys.argv[1])
-num_samples = 100
-np.random.seed(0)
-a = np.random.normal(loc=1, size=(num_samples, 1))
-b = np.random.normal(loc=2, size=(num_samples, 1))
-result = dcor.homogeneity.energy_test(
-    a,
-    b,
-    num_resamples=resamples
-)
-print(result.p_value)
 
+def main():
+    resamples = int(sys.argv[1])
+    num_samples = 100
+    np.random.seed(0)
+    a = np.random.normal(loc=1, size=(num_samples, 1))
+    b = np.random.normal(loc=2, size=(num_samples, 1))
+    result = dcor.homogeneity.energy_test(
+        a,
+        b,
+        num_resamples=resamples
+    )
+    print(result.p_value)
+
+
+if __name__ == '__main__':
+    main()
