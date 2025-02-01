a73e8a2a355ca1b81bdc01c63dba5bbaafd3a10e
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2025-02-01 09:58:01 +0000

    Convert docstring repr expected and actual to numpy 1 style repr

--- conftest.py
+++ conftest.py
@@ -10,20 +10,24 @@ pytest_plugins = ("pytest_cov", "subtests")
 OutputChecker = doctest.OutputChecker
 
 
+numpy2_float64_pattern = r'np\.float64\((?P<num>inf|[+-]?\d*\.\d+)\)'
+
+def ensure_numpy_1_style_repr(repr_: str)-> str:
+    return re.sub(
+                numpy2_float64_pattern,
+                lambda m: m['num'],
+                repr_,
+                )
+
 
 class Numpy1Numpy2AgnosticOutputChecker(doctest.OutputChecker):
 
-    numpy2_float64_pattern = r'np\.float64\((?P<num>inf|[+-]?\d*\.\d+)\)'
-    
     def check_output(self, want, got, optionflags):
 
-        numpy_1_style_got = re.sub(
-                self.numpy2_float64_pattern,
-                lambda m: m['num'],
-                got,
-                )
+        numpy_1_style_want = ensure_numpy_1_style_repr(want)
+        numpy_1_style_got = ensure_numpy_1_style_repr(got)
 
-        return super().check_output(want, numpy_1_style_got, optionflags)
+        return super().check_output(numpy_1_style_want, numpy_1_style_got, optionflags)
 
 
 
